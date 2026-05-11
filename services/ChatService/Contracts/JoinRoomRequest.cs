1f11eba8cb6ea23f2abf25226a5cb05c12cb0c2b
unknown <amit.aditya.1807@gmail.com>
2026-05-11 16:37:47 +0530

    chatservice

--- services/ChatService/Contracts/JoinRoomRequest.cs
+++ services/ChatService/Contracts/JoinRoomRequest.cs
@@ -1,6 +1,8 @@
-﻿namespace ChatService.Contracts
+﻿namespace ChatService.Contracts;
+
+public sealed class JoinRoomRequest
 {
-    public class JoinRoomRequest
-    {
-    }
+    public string RoomKey { get; init; } = string.Empty;
+
+    public string Password { get; init; } = string.Empty;
 }
